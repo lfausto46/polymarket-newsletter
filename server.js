500e7f0070c678fc707455a53f04ef759edd446b
Luis Fausto <luisfausto@Luiss-MBP.hitronhub.home>
2026-05-06 17:14:51 -0400

    serve index from route

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+const fs = require('fs');
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // In-memory storage
 let newsletters = [];
